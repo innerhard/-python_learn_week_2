0d86fc5d6d2fdf63e701c64940920d4d78d22106
Aleksandr Soshnikov <innerhard@gmail.com>
2019-09-21 20:45:17 +0300

    "four part"

--- for_work3.py
+++ for_work3.py
@@ -1,5 +1,6 @@
 students_scores = [{'school_class': '4a', 'scores': [3, 4, 4, 5, 2]},
-                   {'school_class': '2a', 'scores': [3, 4, 4, 5, 3]}, {'school_class': '3a', 'scores': [3, 4, 4, 5, 3]}]
+                   {'school_class': '2a', 'scores': [3, 4, 4, 5, 3]},
+                   {'school_class': '3a', 'scores': [3, 4, 4, 5, 3]}]
 summ_class = 0
 summ_school = 0
 children_number = 0
@@ -8,8 +9,8 @@ for key in students_scores:
     for summ in key['scores']:
         summ_class += summ
         children_number += 1
-    print(
-        f"Средняя оценка класса {key['school_class']} равна {summ_class/len(key['scores'])}")
+    print(f"Средняя оценка класса {key['school_class']} равна \
+            {summ_class/len(key['scores'])}")
     summ_school += summ_class
     summ_class = 0
 print(f"Средняя оценка всех классов {round(summ_school/children_number,1)}")
